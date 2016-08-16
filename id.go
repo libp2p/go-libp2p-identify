aeb90320462f50532da9cd39e4eeed9cbab16e5c
Jeromy <why@ipfs.io>
2016-08-16 13:22:13 -0700

    identify: log protocols from remote peers in peerstore

--- id.go
+++ id.go
@@ -173,6 +173,7 @@ func (ids *IDService) consumeMessage(mes *pb.Identify, c inet.Conn) {
 	p := c.RemotePeer()
 
 	// mes.Protocols
+	ids.Host.Peerstore().SetProtocols(p, mes.Protocols)
 
 	// mes.ObservedAddr
 	ids.consumeObservedAddress(mes.GetObservedAddr(), c)
