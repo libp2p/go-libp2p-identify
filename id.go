9e3d50e82f47b09fdd19ce834ed809cfb2f0328e
Jeromy <why@ipfs.io>
2016-11-18 15:09:18 -0800

    use set protos in identify protocol knowledge gathering

--- id.go
+++ id.go
@@ -193,7 +193,7 @@ func (ids *IDService) consumeMessage(mes *pb.Identify, c inet.Conn) {
 	p := c.RemotePeer()
 
 	// mes.Protocols
-	ids.Host.Peerstore().AddProtocols(p, mes.Protocols...)
+	ids.Host.Peerstore().SetProtocols(p, mes.Protocols...)
 
 	// mes.ObservedAddr
 	ids.consumeObservedAddress(mes.GetObservedAddr(), c)
