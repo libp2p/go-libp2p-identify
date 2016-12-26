1e91546ae38eb086e521f92ed8e194a8fea56105
Jeromy <why@ipfs.io>
2016-12-26 02:52:49 -0800

    identify: make agent version mutable

--- id.go
+++ id.go
@@ -30,7 +30,8 @@ const ID = "/ipfs/id/1.0.0"
 // LibP2PVersion holds the current protocol version for a client running this code
 // TODO(jbenet): fix the versioning mess.
 const LibP2PVersion = "ipfs/0.1.0"
-const ClientVersion = "go-libp2p/3.3.4"
+
+var ClientVersion = "go-libp2p/3.3.4"
 
 // IDService is a structure that implements ProtocolIdentify.
 // It is a trivial service that gives the other peer some
